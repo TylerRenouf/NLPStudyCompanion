d75b4a6e1aa9c0b5ed5ee0d863d9eb4eef903579
TylerRenouf <tyler_renouf123@yahoo.com.au>
2025-03-05 17:10:24 +1000

    Updated .gitignore

--- NLPStudyCompanion.Server/DTOs/UpdateCardDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NLPStudyCompanion.Server.DTOs
-{
-    public class UpdateCardDto
-    {
-        public string? concept { get; set; }
-        public string? content { get; set; }
-        public int? DeckId { get; set; }
-    }
-}
